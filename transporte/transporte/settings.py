c75acef46c8f92fb16389e01f9cd4bd38ce5bf88
AndresC <andrescamachoclaros@gmail.com>
2023-05-12 10:57:45 -0500

    Se pone url permitida de cors

--- transporte/transporte/settings.py
+++ transporte/transporte/settings.py
@@ -143,8 +143,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
-    '*'
+    'http://localhost:4200/'
 )
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200'
+]
 CORS_ALLOW_METHODS = (
     'DELETE',
     'GET',
